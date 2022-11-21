0fc3c74a1c8e7f53033a5f7d0c1550ea5c3ca665
olga-plotkina <olga.semenyuta@gmail.com>
2022-11-21 23:53:59 +0000

    add oneMovieInfo

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,8 +1,63 @@
+import Notiflix from 'notiflix';
 import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import { getMovieById } from 'api/getMovieById';
-
+import { Link } from 'react-router-dom';
+import {
+  MovieInformation,
+  MovieCard,
+  AdditionalInfo,
+} from './MovieDetails.styled';
 export const MovieDetails = () => {
+  const [movieInfo, setMovieInfo] = useState([]);
+  const [movieGenres, setMovieGenres] = useState('');
+  const [poster, setPoster] = useState(
+    'https://media.istockphoto.com/vectors/cat-sits-in-a-box-with-a-404-sign-page-or-file-not-found-connection-vector-id1278808623?k=20&m=1278808623&s=612x612&w=0&h=tmzYgVK5yF-dtVvW81zz-Ebpeqd6EvD38KYGRjczuiw='
+  );
+  const [userScore, setUserScore] = useState('0');
   const { movieId } = useParams();
-  console.log(getMovieById(movieId));
-  return <div>Now showing product-{movieId} </div>;
+
+  useEffect(() => {
+    async function getMovieInfo() {
+      try {
+        const movie = await getMovieById(movieId);
+        console.log(movie);
+        setMovieInfo(movie);
+        setMovieGenres(movie.genres.map(genre => genre.name).join(', '));
+        setPoster(`https://image.tmdb.org/t/p/w500${movie.poster_path}`);
+        setUserScore(`${(movie.vote_average * 10).toFixed(0)}%`);
+      } catch (error) {
+        Notiflix.Notify.failure(error);
+      }
+    }
+    getMovieInfo();
+  }, [movieId]);
+
+  return (
+    <main>
+      <button type="button">Go back</button>
+      <MovieInformation>
+        <img src={poster} alt="King" />
+        <MovieCard>
+          <h2>{movieInfo.title}</h2>
+          <span>User score: {userScore}</span>
+          <h3>Overview</h3>
+          <p>{movieInfo.overview}</p>
+          <h3>Genres</h3>
+          {movieGenres && <p>{movieGenres}</p>}
+        </MovieCard>
+      </MovieInformation>
+      <AdditionalInfo>
+        Additional information
+        <ul>
+          <li>
+            <Link>Cast</Link>
+          </li>
+          <li>
+            <Link>Review</Link>
+          </li>
+        </ul>
+      </AdditionalInfo>
+    </main>
+  );
 };
---
+++ src/pages/MovieDetails.styled.jsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const MovieInformation = styled.div`
+  margin-left: 15px;
+  display: flex;
+
+  img {
+    width: 200px;
+  }
+`;
+
+export const MovieCard = styled.div`
+  padding: 5px 15px;
+`;
+
+export const AdditionalInfo = styled.div`
+  margin: 10px 15px;
+
+  li {
+    margin-bottom: 5px;
+  }
+`;
